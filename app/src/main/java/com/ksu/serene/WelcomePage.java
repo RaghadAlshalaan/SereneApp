d0bdbd6d57d6efc0152b4accec2aefdf883b94e3
lama-998 <lama-almarshad@hotmail.com>
2020-01-23 00:58:02 +0300

    WelcomePage + logo in drawable

---
+++ app/src/main/java/com/ksu/serene/WelcomePage.java
@@ -0,0 +1,39 @@
+package com.ksu.serene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomePage extends AppCompatActivity {
+
+    private Button logIn;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+        logIn = findViewById(R.id.LOGIN);
+        register = findViewById(R.id.REGISTER);
+
+        logIn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(WelcomePage.this, logIn.class);
+                //startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(WelcomePage.this, register.class);
+                //startActivity(intent);
+            }
+        });
+
+    }
+}
