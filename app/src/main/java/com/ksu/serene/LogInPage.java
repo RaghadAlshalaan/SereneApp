7187f73b0b1d17c4f6c5c7cc8a33cc164eb061d6
Rima AlSadhan <RimaSadhan@gmail.com>
2020-02-19 13:07:13 +0300

    [FIX] : login page

--- app/src/main/java/com/ksu/serene/LogInPage.java
+++ app/src/main/java/com/ksu/serene/LogInPage.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
@@ -29,88 +33,214 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.ksu.serene.Controller.Signup.Signup;
-import com.ksu.serene.Controller.Homepage.Home.HomeFragment;
 import com.ksu.serene.Model.Token;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Thread.sleep;
+
 //this is the controller
-public class LogInPage extends AppCompatActivity {
+public class LogInPage extends AppCompatActivity implements View.OnClickListener {
 
     private EditText email;
     private EditText password;
-    private Button logIn;
-    private TextView signup;
+    private Button loginBtn;
+    private TextView signup , Error;
     private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private String emailIn, passwordIn, mToken;
     private String TAG = LogInPage.class.getSimpleName();
+    private TextView forgotPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
+        getSupportActionBar().hide();
+
         email = findViewById(R.id.Email);
         password = findViewById(R.id.Password);
-        logIn = findViewById(R.id.Log_In);
-        signup = findViewById(R.id.sign_up);
+        loginBtn = findViewById(R.id.loginBtn);
+        signup = findViewById(R.id.registerTV);
+        forgotPassword = findViewById(R.id.forgetPassword);
+        forgotPassword.setOnClickListener( this );
+        Error = findViewById(R.id.Error);
+        Error.setText("");
+        signup.setOnClickListener ( this );
+        loginBtn.setOnClickListener( this );
 
-        signup.setOnClickListener ( new View.OnClickListener() {
-            @Override
-             public void onClick(View view) {
-                Intent intent = new Intent(LogInPage.this, Signup.class);
-                startActivity(intent);
-              }
-         }
-        );
+        // TODO: do the same as sign up button
 
-        logIn.setOnClickListener( new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                emailIn = email.getText().toString();
-                passwordIn = password.getText().toString();
-                //check all fields not empty
-                if (checkAllFields(emailIn , passwordIn)) {
+        email.addTextChangedListener(loginTextWatcher);
+        password.addTextChangedListener(loginTextWatcher);
 
-                    mAuth.signInWithEmailAndPassword(emailIn, passwordIn)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        checkIfEmailVerified();
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d(TAG, "signInWithEmail:success");
 
-                                    } else {
-                                        if (task.getException() instanceof FirebaseAuthInvalidUserException) {
-                                            //there is'n user with this Email
-                                            Toast.makeText(LogInPage.this, R.string.emailpassWrong,
-                                                    Toast.LENGTH_SHORT).show();
 
-                                        } else if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                            //the password is wrong
-                                            Toast.makeText(LogInPage.this, R.string.emailpassWrong,
-                                                    Toast.LENGTH_SHORT).show();
+    }// end onCreate
 
-                                        } else {
-                                            // If sign in fails, display a message to the user.
-                                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                            Toast.makeText(LogInPage.this, R.string.auth,
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
 
-                                    // ...
-                                }
-                            });
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), R.string.allFields, Toast.LENGTH_LONG).show();
-                    return;
-                }
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            String emailInput = email.getText().toString().trim();
+            String passwordInput = password.getText().toString().trim();
+
+            if( !emailInput.equals("") & !passwordInput.equals("") ){
+
+                loginBtn.setBackgroundTintList(getResources().getColorStateList(R.color.colorAccent));
+
+                loginBtn.setEnabled(true);
+
+            }else{
+                loginBtn.setBackgroundTintList(getResources().getColorStateList(R.color.Grey));
+
+                loginBtn.setEnabled(false);
             }
-        });
+        }
 
-      }
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.loginBtn:
+
+                login( email.getText().toString(), password.getText().toString() );
+
+                break;
+
+            case R.id.forgetPassword:
+
+                forgotPassword();
+
+                break;
+
+            case R.id.registerTV:
+
+                Intent i = new Intent(LogInPage.this, Signup.class);
+                startActivity(i);
+
+                break;
+
+            default:
+                break;
+        }//end switch
+
+    }
+
+    private void forgotPassword() {
+
+        final android.app.AlertDialog.Builder resetPasswordDialog = new android.app.AlertDialog.Builder(LogInPage.this);
+        resetPasswordDialog.setTitle("Reset Password");
+
+        final EditText forgetEmailET = new EditText(LogInPage.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        forgetEmailET.setLayoutParams(lp);
+
+        resetPasswordDialog.setMessage("If you do not know your current password,\nyou may change it.\nPlease enter your email");
+
+        resetPasswordDialog.setView(forgetEmailET);
+
+        resetPasswordDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+        });//end setNegativeButton
+
+        resetPasswordDialog.setPositiveButton("Request New Password", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if ( forgetEmailET.getText().toString().equals("") || !forgetEmailET.getText().toString().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")) {
+                    Toast.makeText(getApplication(), "Please, enter your email correctly.", Toast.LENGTH_SHORT).show();
+                    forgotPassword();
+
+                } else resetPassword(forgetEmailET.getText().toString());
+
+            }
+        });//end setPositiveButton
+
+        resetPasswordDialog.show();
+
+    }
+
+    private void resetPassword(String email){
+
+        mAuth.sendPasswordResetEmail(email)
+
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LogInPage.this, "Email has been sent to reset your password!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LogInPage.this, "Failed to send reset email! Try again in moments.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    private void login(String emailIn, String passwordIn) {
+
+        mAuth.signInWithEmailAndPassword(emailIn, passwordIn)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            checkIfEmailVerified();
+
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+
+                        } else {
+
+                            if (task.getException() instanceof FirebaseAuthInvalidUserException) {
+                                //there is'n user with this Email
+                                Error.setText("Failed:\n - There is not user with this Email.\nPlease try again");
+
+
+                            } else if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                //the password is wrong
+                                Error.setText("Failed:\n - Wrong Password.\nPlease try again");
+
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LogInPage.this, R.string.auth,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                    }
+                });
+
+
+
+    }
 
     private void checkIfEmailVerified() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -142,18 +272,19 @@ public class LogInPage extends AppCompatActivity {
 
             FirebaseAuth.getInstance().signOut();
 
-            //restart this activity
+            //TODO : restart this activity? REMOVE IT?
+
 
         }
-    }
+    }// end checkIfEmailVerified
+
     public  void updateToken(String token){
 
         DocumentReference userTokenDR = FirebaseFirestore.getInstance().collection("Tokens").document(mAuth.getUid());
         Token mToken = new Token(token);
         final Map<String, Object> tokenh = new HashMap<>();
         tokenh.put("token",mToken.getToken());
-        // Set the "isCapital" field of the city 'DC'
-        //
+
         userTokenDR
                 .update(tokenh)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -167,11 +298,10 @@ public class LogInPage extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error updating document", e);
-
-
                     }
                 });
     }
+
     private void checkUserState() {
 
         if (mAuth.getCurrentUser() != null) {
@@ -202,4 +332,5 @@ public class LogInPage extends AppCompatActivity {
         super.onStart();
         checkUserState();
     }
+
 }
